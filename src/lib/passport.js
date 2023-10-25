1e39b0c0ddf453eb6290888a9bc01cb79ddb31ef
Miguel <miguelnavas933@gmail.com>
2023-10-25 11:58:53 -0500

    cambios

--- src/lib/passport.js
+++ src/lib/passport.js
@@ -38,7 +38,7 @@ passport.use('local.signup', new LocalStrategy({
   newUser.password = await helpers.encryptPassword(password);
   // Saving in the Database
   const result = await pool.query('INSERT INTO users SET ? ', newUser);
-  newUser.id = result.insertId;
+  newUser.u_id = result.insertId;
   return done(null, newUser);
 }));
 
